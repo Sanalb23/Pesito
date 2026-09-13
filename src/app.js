0dd0d8dcb6b9541170cb880ce8f046ae1a164490
Santino Albornoz <sanalbor32@gmail.com>
2026-09-12 23:35:23 -0300

    Implementar JWT en cookies para login y registro

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const cookieParser = require('cookie-parser');
 
 require('dotenv').config();
 
 app.use(express.json());
+app.use(cookieParser());
 
 const authRoutes = require("./routes/auth.routes");
 
