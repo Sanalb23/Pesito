0c232d173d6a7ee13b3744fafb0c8c15bd2bc896
Santino Albornoz <sanalbor32@gmail.com>
2026-09-22 22:29:30 -0300

    Revert "Agregar servicio y endpoint para resolución de nombres de equipos"
    This reverts commit 8b7a47d30a5bb3bb0b2675f63aaea28da997fb32.

--- src/models/team.game.model.js
+++ src/models/team.game.model.js
@@ -13,21 +13,6 @@ const getTeamsByLeagueAndGame = async (gameId, leagueId) => {
     return result.rows;
 };
 
-const findTeamByNameAndGame = async (gameId, teamName) => {
-    const query = `
-        SELECT tg.id, t.name
-        FROM teams t
-        INNER JOIN teams_games tg
-        ON t.id = tg.team_id
-        WHERE game_id = $1
-        ORDER BY SIMILARITY(t.name, $2) DESC
-        LIMIT 1`;
-
-    const result = await pool.query(query, [gameId, teamName]);
-    return result.rows[0];
-};
-
 module.exports = {
-    getTeamsByLeagueAndGame,
-    findTeamByNameAndGame
+    getTeamsByLeagueAndGame
 };
--- src/routes/recognition.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { resolveMatchData } = require("../controllers/recognition.controller");
-const { verifyToken } = require("../middlewares/auth");
-
-const router = express.Router();
-
-router.post("/resolve", verifyToken, resolveMatchData);
-
-module.exports = router;
